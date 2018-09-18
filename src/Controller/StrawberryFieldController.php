9ecf41a50db943783ab4cf6c3853a1dc673fd6ba
Favenzio <fcalvo@fsu.edu>
2018-09-18 16:15:20 -0400

    Create StrawberryFieldController.php

---
+++ src/Controller/StrawberryFieldController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\strawberryfield\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Defines StrawberryFieldController class.
+ */
+class StrawberryFieldController extends ControllerBase {
+
+}
