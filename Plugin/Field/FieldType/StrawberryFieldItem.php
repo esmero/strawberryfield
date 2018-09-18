3734577e1671cb57f9a1a75772d47503c2c2e5e9
Favenzio <fcalvo@fsu.edu>
2018-09-18 16:53:55 -0400

    Create StrawberryFieldItem.php

---
+++ Plugin/Field/FieldType/StrawberryFieldItem.php
@@ -0,0 +1,15 @@
+/**
+ * Provides a field type of strawberry field.
+ * 
+ * @FieldType(
+ *   id = "strawberry",
+ *   label = @Translation("Strawberry Field"),
+ *   description = @Translation("This field stores JSON text."),
+ *   default_formatter = "strawberry_formatter",
+ *   default_widget = "strawberry_widget",
+ * )
+ */
+ class StrawberryFieldItem extends FieldItemBase implements FieldItemInterface {
+ 
+ }
+ 
