234d3ce3cf7501c6abc3b1edd98c1222162f89af
Diego Pino Navarro <dpino@metro.org>
2020-06-23 16:02:38 -0400

    Adds permissions to View RAW JSON metadata
    Now that we extract EXIF and there can be a lot of fun and private stuff in EXIF data we can / should allow a permission on the RAW display
    
    This adds 2 new permissions that only apply to the StrawberryDefaultFormatter
    View any raw JSON stored in a Strawberryfield
    View own raw JSON in a Strawberryfield
    
    This is how things work here:
    - if the entity (node, file, etc) has an owner the 'Own' can be actually evaluated
    - every user can have the 'View any' of course
    - Every user that can edit an entity can see the RAW metadata (i see no benefit on hiding data with editing capabillities to be honest)
    Users with 'bypass node access' can see everything as always.
    
    TODO: of course a lot, we can maybe make this a more general method. I can also inject the user here instead of using the drupal:: service. But then, i don't think its crucial here right now.

--- src/Plugin/Field/FieldFormatter/StrawberryDefaultFormatter.php
+++ src/Plugin/Field/FieldFormatter/StrawberryDefaultFormatter.php
@@ -10,6 +10,9 @@ namespace Drupal\strawberryfield\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\user\EntityOwnerInterface;
+use Drupal\Core\Entity\ContentEntityBase;
 
 /**
  * Simplistic Strawberry Field formatter.
@@ -51,11 +54,32 @@ class StrawberryDefaultFormatter extends FormatterBase {
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $element = [];
-    $entity = $items->getEntity();
-    $access = $entity
-      ->access('edit', NULL, TRUE)->isAllowed();
+    $account = $this->prepareUser();
 
-    if ($access) {
+    // Only enforce owner permissions here if the entity has an owner
+    $entity = $items->getEntity();
+    $access = FALSE;
+
+    //@TODO make this its own Method and reuse in every Formatter.
+    if ($entity instanceof EntityOwnerInterface) {
+      /* @var ContentEntityBase $entity */
+      // Check if the entity can have an owner?
+
+      if (($account->id() == $entity->getOwner()->id()) &&
+        $account->hasPermission('view own Raw Strawberryfield')) {
+        $access = TRUE;
+      }
+      elseif ($account->hasPermission('view any Raw Strawberryfield')) {
+        $access = TRUE;
+      }
+    }
+    else {
+      // Users with Edit access can always see this?
+      $access = $entity
+        ->access('edit', NULL, TRUE)->isAllowed();
+    }
+
+    if ($access || $account->hasPermission('bypass node access')) {
       foreach ($items as $delta => $item) {
         // Render each element as markup.
         $element[$delta] = [
@@ -69,11 +93,27 @@ class StrawberryDefaultFormatter extends FormatterBase {
             ),
             '#prefix' => '<pre>',
             '#suffix' => '</pre>',
-          ]
+          ],
         ];
       }
     }
     return $element;
+  }
+
+  /**
+  * Loads the current account object, if it does not exist yet.
+  *
+  * @param \Drupal\Core\Session\AccountInterface $account
+  *   The account interface instance.
+  *
+  * @return \Drupal\Core\Session\AccountInterface
+  *   Returns the current account object.
+  */
+  protected function prepareUser(AccountInterface $account = NULL) {
+    if (!$account) {
+      $account = \Drupal::currentUser();
+    }
+    return $account;
   }
 
 
