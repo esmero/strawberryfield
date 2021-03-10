d27c42c7bb9edd50cc382a106063917a6f292092
Pedro Cambra <pedro@cambra.io>
2021-03-10 10:34:31 +0100

    ISSUE-123: Addressed feedback

--- src/EventSubscriber/StrawberryEventSaveFileDeleteFlavorSubscriber.php
+++ src/EventSubscriber/StrawberryEventSaveFileDeleteFlavorSubscriber.php
@@ -3,7 +3,6 @@
 namespace Drupal\strawberryfield\EventSubscriber;
 
 use Drupal\Core\Entity\EntityInterface;
-use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\file\FileInterface;
 use Drupal\search_api\Utility\Utility;
 use Drupal\strawberryfield\Event\StrawberryfieldCrudEvent;
@@ -18,6 +17,11 @@ class StrawberryEventSaveFileDeleteFlavorSubscriber extends StrawberryfieldEvent
   /**
    * {@inheritdoc}
    */
+  protected static $priority = 100;
+
+  /**
+   * {@inheritdoc}
+   */
   public function onEntitySave(StrawberryfieldCrudEvent $event) {
     // We need to compare the original entity with the saved just now as file
     // events don't really work for this use case. The file is still attached to
@@ -25,83 +29,39 @@ class StrawberryEventSaveFileDeleteFlavorSubscriber extends StrawberryfieldEvent
     // the isNew() possibility here.
     $entity = $event->getEntity();
     $original_entity = $entity->original;
-    $sbf_fields = $event->getFields();
-    $indexes = StrawberryfieldFlavorDatasource::getValidIndexes();
 
     // We only care about the original entity ones, so that's the system of
     // record to compare. If $files_deleted is empty, it means new files, or no
-    // deletions so we don't care.
-    $entity_documents = $this->getDocuments($entity, $sbf_fields);
-    $original_entity_documents = $this->getDocuments($original_entity, $sbf_fields);
-    $files_deleted = array_diff($original_entity_documents, $entity_documents);
+    // deletions so we don't treat those.
+    $entity_files = array_column($entity->get('field_file_drop')->getValue() ?? [], 'target_id');
+    $original_entity_files = array_column($original_entity->get('field_file_drop')->getValue() ?? [], 'target_id');
+    $files_deleted = array_diff($original_entity_files, $entity_files);
 
     foreach ($files_deleted as $file_id) {
       $file = OcflHelper::resolvetoFIDtoURI($file_id);
-      // This is very important as we need to compare with the CURRENT revision,
-      // default looks for historical so there will be always references. In any
-      // case, in the unlikely event that this same file is reused accross the
-      // system, we bail out.
-      $references = file_get_file_references($file, NULL, EntityStorageInterface::FIELD_LOAD_CURRENT);
-      if (empty($references)) {
-        $this->trackDocumentsDeleted($indexes, $file);
-      }
+      // No need to review if the file is being used somewhere else as we're
+      // removing the tracking contextually to the entity.
+      $this->trackFilesDeleted($entity, $file);
     }
-  }
-
-  /**
-   * Gets the documents from an entity.
-   *
-   * @param \Drupal\Core\Entity\EntityInterface $entity
-   *   The entity.
-   * @param array $sbf_fields
-   *   Array of SBF fields.
-   *
-   * @return array
-   *   Array of document ids belonging to the entity.
-   */
-  protected function getDocuments(EntityInterface $entity, array $sbf_fields) {
-    $documents = [];
-    // @TODO get this from the field config instead?
-    $key = 'as:document';
-    foreach ($sbf_fields as $field_name) {
-      /** @var \Drupal\strawberryfield\Field\StrawberryFieldItemList $field */
-      $field = $entity->get($field_name);
-      if (!$field->isEmpty()) {
-        foreach ($field->getIterator() as $delta => $item) {
-          // @TODO we could benefit of a getFile method on the SBF or at least
-          // a static.
-          $jsondata = json_decode($item->value, true);
-          if (!empty($jsondata[$key])) {
-            foreach ($jsondata[$key] as $mediaitem) {
-              if (isset($mediaitem['type']) && $mediaitem['type'] == 'Document') {
-                if (isset($mediaitem['dr:fid'])) {
-                  $documents[$mediaitem['dr:fid']] = $mediaitem['dr:fid'];
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-
-    return $documents;
   }
 
   /**
    * Deletes the documents tracked on Search Api.
    *
-   * @param \Drupal\search_api\IndexInterface[] $indexes
-   *   Array of contextual Search Api indexes for SBF.
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity to delete flavors contextually.
    * @param \Drupal\file\FileInterface $file
-   *   The file associated to the document.
+   *   The file associated to the entity.
+   *
+   * @throws \Drupal\search_api\SearchApiException
    */
-  protected function trackDocumentsDeleted(array $indexes, FileInterface $file) {
+  protected function trackFilesDeleted(EntityInterface $entity, FileInterface $file) {
     $datasource_id = 'strawberryfield_flavor_datasource';
-    foreach ($indexes as $index) {
+    foreach (StrawberryfieldFlavorDatasource::getValidIndexes() as $index) {
       $query = $index->query(['offset' => 0]);
       $query->addCondition('file_uuid', $file->uuid())
         ->addCondition('search_api_datasource', $datasource_id)
-        ->addCondition('processor_id', 'ocr');
+        ->addCondition('uuid', $entity->uuid());
       $results = $query->execute();
 
       if ($results->getResultCount() > 0) {
