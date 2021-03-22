a63f96807020f38fbd73e8096f47fb751090bbf1
Pedro <pcambra@users.noreply.github.com>
2021-03-22 15:39:58 +0100

    ISSUE-123: Add SOLR SBF Flavor Document removal when File gets removed (#162)
    * ISSUE-123: Add SOLR SBF Flavor Document removal when File gets removed
    
    * ISSUE-123: Addressed feedback

--- src/Field/StrawberryFieldFileComputedItemList.php
+++ src/Field/StrawberryFieldFileComputedItemList.php
@@ -5,25 +5,29 @@ namespace Drupal\strawberryfield\Field;
 use Drupal\file\Plugin\Field\FieldType\FileFieldItemList;
 use Drupal\Core\TypedData\ComputedItemListTrait;
 
-
+/**
+ * Class StrawberryFieldFileComputedItemList.
+ *
+ * @package Drupal\strawberryfield\Field
+ */
 class StrawberryFieldFileComputedItemList extends FileFieldItemList {
 
   use ComputedItemListTrait;
 
+  /**
+   * {@inheritdoc}
+   */
   protected function computeValue() {
-    $sbf_fields = \Drupal::service('strawberryfield.utility')
-      ->bearsStrawberryfield($this->getEntity());
+    $sbf_fields = \Drupal::service('strawberryfield.utility')->bearsStrawberryfield($this->getEntity());
     foreach ($sbf_fields as $field_name) {
-      /* @var $field \Drupal\Core\Field\FieldItemInterface */
+      /** @var \Drupal\strawberryfield\Field\StrawberryFieldItemList $field */
       $field = $this->getEntity()->get($field_name);
       if (!$field->isEmpty()) {
-        foreach ($field->getIterator() as $delta => $itemfield) {
+        foreach ($field->getIterator() as $itemfield) {
           // Note: we are not longer touching the metadata here.
-          /** @var $itemfield \Drupal\strawberryfield\Plugin\Field\FieldType\StrawberryFieldItem */
+          /** @var \Drupal\strawberryfield\Plugin\Field\FieldType\StrawberryFieldItem $itemfield */
           $flatvalues = (array) $itemfield->provideFlatten();
-          if (isset($flatvalues['dr:fid']) && is_array(
-              $flatvalues['dr:fid']
-            ) && !empty($flatvalues['dr:fid'])) {
+          if (isset($flatvalues['dr:fid']) && is_array($flatvalues['dr:fid']) && !empty($flatvalues['dr:fid'])) {
             $file_entities = array_filter($flatvalues['dr:fid'], 'is_int');
             foreach ($file_entities as $delta => $id) {
               $this->list[$delta] = $this->createItem($delta, $id);
@@ -33,4 +37,5 @@ class StrawberryFieldFileComputedItemList extends FileFieldItemList {
       }
     }
   }
+
 }
