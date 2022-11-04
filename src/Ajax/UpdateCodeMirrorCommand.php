4af3288c730bfdc66e62c57396503746a20a3dfa
aksm <amin@metro.org>
2022-11-04 11:45:34 -0400

    Migrate ADO Tools from strawberry_runners module and update references to previous module.

---
+++ src/Ajax/UpdateCodeMirrorCommand.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dpino
+ * Date: 10/1/19
+ * Time: 11:30 AM
+ */
+
+namespace Drupal\strawberryfield\Ajax;
+use Drupal\Core\Ajax\CommandInterface;
+class UpdateCodeMirrorCommand implements CommandInterface
+{
+
+  /**
+   * The Content that will be updated on the Code Mirror text area
+   *
+   * @var \stdClass;
+   */
+  protected $content;
+
+  /**
+   * The JQuery() selector
+   *
+   * @var string
+   */
+  protected $selector;
+
+  /**
+   * Constructs an AlertCommand object.
+   *
+   * @param string $text
+   *   The text to be displayed in the alert box.
+   */
+  public function __construct($selector, $content) {
+    $this->selector = $selector;
+    $this->content = $content;
+
+  }
+
+  /**
+   * Implements Drupal\Core\Ajax\CommandInterface:render().
+   */
+  public function render() {
+
+    return [
+      'command' => 'strawberry_runners_codemirror',
+      'selector' => $this->selector,
+      'content' => $this->content,
+    ];
+  }
+}
