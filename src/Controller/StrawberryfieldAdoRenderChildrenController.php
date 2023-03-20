4e969cd8af19e730df29db2ad25a046dd39f190b
aksm <amin@metro.org>
2023-03-20 11:20:17 -0400

    Add Controller, tasks, and routes for ADO tools submenus.

---
+++ src/Controller/StrawberryfieldAdoRenderChildrenController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\strawberryfield\Controller;
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Render\Renderer;
+use Drupal\views\Views;
+
+/**
+ * A Controller for rendering a view of an ADO's children.
+ */
+class StrawberryfieldAdoRenderChildrenController extends ControllerBase {
+  
+    /**
+   * Renders the contestants page.
+   *
+   * @return array
+   *   The renderable array.
+   */
+  public function renderAdoChildrenViews() {
+    $view_names = ['ado_tools_children', 'ado_tools_children_creative_work_series'];
+    $return = [];
+    foreach ($view_names as $view_name) {
+      $view = Views::getView($view_name);
+      if(isset($view)) {
+        $view->execute();
+        $rendered = $view->render();
+        if(!empty($rendered['#rows'])) {
+          $output = \Drupal::service('renderer')->render($rendered);
+          $markup = ['#markup' => $output];
+          array_push($return, $markup);
+        }
+      }
+    }
+    if(!empty($return)) {
+      return $return;
+    } else {
+      \Drupal::logger('strawberryfield')
+      ->notice('To view ADO tools children create views called ado_tools_children and/or ado_tools_children_creative_work_series.');
+      return $return;
+    }
+  }
+}
