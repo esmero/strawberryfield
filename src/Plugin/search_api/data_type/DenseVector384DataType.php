11f62bca34466b107922fd64ca46aee66ca4330a
Diego Pino Navarro <DiegoPino@users.noreply.github.com>
2024-05-14 16:07:29 -0400

    Custom data types, one per vector size, one to rule them all

---
+++ src/Plugin/search_api/data_type/DenseVector384DataType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\strawberryfield\Plugin\search_api\data_type;
+
+use Drupal\search_api\DataType\DataTypePluginBase;
+
+/**
+ * Provides a Vector data type for 384 length search api fields.
+ *
+ * @SearchApiDataType(
+ *   id = "densevector_384",
+ *   label = @Translation("Dense Vector of 384 length"),
+ *   description = @Translation("Contains Dense Vectors, float values."),
+ *   fallback_type = "decimal",
+ *   prefix = "knn384"
+ * )
+ */
+class DenseVector384DataType extends DataTypePluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getValue($value) {
+    $value = (float) $value;
+    return $value;
+  }
+
+}
---
+++ src/Plugin/search_api/data_type/DenseVector512DataType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\strawberryfield\Plugin\search_api\data_type;
+
+use Drupal\search_api\DataType\DataTypePluginBase;
+
+/**
+ * Provides a Vector data type for 512 length search api fields.
+ *
+ * @SearchApiDataType(
+ *   id = "densevector_512",
+ *   label = @Translation("Dense Vector of 512 length"),
+ *   description = @Translation("Contains Dense Vectors, float values."),
+ *   fallback_type = "decimal",
+ *   prefix = "knn512"
+ * )
+ */
+class DenseVector512DataType extends DataTypePluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getValue($value) {
+    $value = (float) $value;
+    return $value;
+  }
+
+}
